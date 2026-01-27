7137172e6be9e95a07fbc7c642c3c94a1456ea2d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-28 01:16:55 +0600

    another array or while loop connect

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ console.log('hi');
 
 const numbers = [1,2,3,4,5,6,7];
 console.log(numbers.reverse());
+// array and loop connected
+const fruits = ['apple','banana','painaple','carrote','angur'];
+let i = 0;
+while(i < fruits.length){
+    console.log(fruits[i]);
+    i++
+}
